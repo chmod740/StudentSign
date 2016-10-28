7f2edd979309ff0bd320f3bec3344b75bbcf4bea
hupeng <hupeng@imudges.com>
2016-10-28 15:13:54 +0800

    update

--- web/models.py
+++ web/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Student(models.Model):
     stu_num = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    gender = models.IntegerField(default=0)
+    college = models.CharField(default='')
+    phone = models.CharField(default='')
+    weixin = models.CharField(default='')
+    qq = models.CharField(default='')
