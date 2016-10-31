6eed53db1f16447c6b9de2583d7e9876ed3a7ca5
HUPENG <hupeng@imudges.com>
2016-10-31 21:30:18 +0800

    update

--- web/views.py
+++ web/views.py
@@ -118,6 +118,7 @@ def logout(req):
 
 def student(req):
     is_teacher = req.session.get('teacher')
+    student_obj = Student.objects.get(stu_num__exact=req.session.get('username'))
     if is_teacher is None:
         return HttpResponseRedirect('login.html')
     if is_teacher:
@@ -131,7 +132,7 @@ def student(req):
         ts = time.mktime(sign.sign_in_time.timetuple())
         ts = int(ts)
         ts = ts + 8 * 60 *60
-    return render_to_response('student.html', {'teachers': teachers, 'sign': sign, 'ts': ts})
+    return render_to_response('student.html', {'teachers': teachers, 'sign': sign, 'ts': ts, 'student':student_obj})
 
 def teacher(req):
     is_teacher = req.session.get('teacher')
@@ -145,16 +146,13 @@ def teacher(req):
 def sign_in(req):
     try:
         student_obj = Student.objects.get(stu_num__exact=req.session.get('username'))
-
         f = datetime(2000, 1, 1, 0, 0)
         t = datetime(2000, 1, 2, 0, 0)
-
         signs = Sign.objects.filter(sign_off_time__range=(f,t))
         if len(signs) == 0:
             teacher_num = req.POST.get('teacher_num')
             teachers = Teacher.objects.filter(teacher_num__exact=teacher_num)
             teacher_obj = teachers[0]
-
             sign = Sign()
             sign.teacher = teacher_obj
             sign.student = student_obj
@@ -164,5 +162,22 @@ def sign_in(req):
     except:
         pass
     return HttpResponseRedirect('student.html')
-
+@csrf_exempt
+def sign_off(req):
+    remark = req.POST.get('remark')
+    try:
+        student_obj = Student.objects.get(stu_num__exact=req.session.get('username'))
+        f = datetime(2000, 1, 1, 0, 0)
+        t = datetime(2000, 1, 2, 0, 0)
+        signs = Sign.objects.filter(sign_off_time__range=(f, t))
+        if not len(signs) == 0:
+            teacher_num = req.POST.get('teacher_num')
+            teachers = Teacher.objects.filter(teacher_num__exact=teacher_num)
+            teacher_obj = teachers[0]
+            sign = signs[0]
+            sign.sign_off_time = datetime.now()
+            sign.remark = remark
+            sign.save()
+    except:
+        pass
 
