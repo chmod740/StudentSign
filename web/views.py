3e60c13387935c10713d3238c916ea2b22ff3650
HUPENG <hupeng@imudges.com>
2016-11-03 13:32:27 +0800

    update

--- web/views.py
+++ web/views.py
@@ -146,15 +146,16 @@ def student(req):
         ts = time.mktime(sign.sign_in_time.timetuple())
         ts = int(ts)
         sysstr = platform.system()
-        if (sysstr == "Windows"):
-            ts = ts + 8 * 60 * 60
+        # if (sysstr == "Windows"):
+        #     ts = ts + 8 * 60 * 60
+        ts = ts + 8 * 60 * 60
     if date is None or date == '':
         page_size = 10
         f = datetime(2001, 1, 1, 0, 0, 26, 423063)
         t = datetime.now()
         signs = Sign.objects.order_by('-id').filter(student__exact=student_obj, sign_off_time__range=(f, t))[
                 page * page_size: page * page_size + page_size]
-        sign_size = len(Sign.objects.all())
+        sign_size = len(Sign.objects.filter(student__exact=student_obj))
         page_range = int((sign_size + page_size - 1) / page_size)
     else:
         d1 = datetime.strptime(date, "%Y-%m-%d")
@@ -219,11 +220,12 @@ def teacher(req):
         if ts_end < ts_start:
             ts_end = time.mktime(datetime.now().timetuple())
         else:
-            if sysstr == "Windows":
-                ts_end = ts_end + 8 * 60 * 60
-        if sysstr == "Windows":
-            ts_start = ts_start + 8 * 60 * 60
-
+            # if sysstr == "Windows":
+            #     ts_end = ts_end + 8 * 60 * 60
+            ts_end = ts_end + 8 * 60 * 60
+        # if sysstr == "Windows":
+        #     ts_start = ts_start + 8 * 60 * 60
+        ts_start = ts_start + 8 * 60 * 60
         ts_diff = ts_end - ts_start
         my_sign.time_diff = get_format_time(ts_diff)
     return render_to_response('teacher.html', {'teacher': teacher_obj, 'signs': my_signs})
@@ -267,3 +269,79 @@ def sign_off(req):
     except:
         pass
     return render_to_response('black.html', {'msg': '签离成功', 'location': 'student.html'})
+
+@csrf_exempt
+def change_student_info(req):
+    is_teacher = req.session.get('teacher')
+    if is_teacher is None or is_teacher:
+        return render_to_response('black.html', {'msg': '请登录', 'location': 'login.html'})
+    username = req.session.get('username')
+    try:
+        student_obj = Student.objects.get(stu_num__exact=username)
+    except:
+        return render_to_response('black.html', {'msg': '登录信息发生错误，请重新登录', 'location': 'login.html'})
+
+    if req.method == 'GET':
+        return render_to_response('change_student_info.html', {'student': student_obj})
+    else:
+        username = req.POST.get('username')
+        name = req.POST.get('name')
+        gender = req.POST.get('gender')
+        password = req.POST.get('password')
+        re_password = req.POST.get('re_password')
+        college = req.POST.get('college')
+        phone = req.POST.get('phone')
+        weixin = req.POST.get('weixin')
+        qq = req.POST.get('qq')
+        work_location = req.POST.get('work_location')
+        work_character = req.POST.get('work_character')
+        if not password == re_password:
+            return render_to_response('change_student_info.html', {'msg': '两次输入的密码不一致'})
+        # students = Student.objects.filter(stu_num__exact=username)
+        # if not len(students) == 0:
+        #     return render_to_response('change_student_info.html.html', {'msg': '用户已经存在'})
+        if gender == '' or username == '' or name == '' or college == '' or phone == '' or weixin == '' or qq == '' or work_location == '' or work_character == '':
+            return render_to_response('change_student_info.html', {'msg': '所有项均为必填项'})
+        student_obj.name = name
+        student_obj.gender = int(gender)
+        if not password == '':
+            student_obj.password = password
+        student_obj.college = college
+        student_obj.phone = phone
+        student_obj.weixin = weixin
+        student_obj.qq = qq
+        student_obj.work_location = work_location
+        student_obj.work_character = work_character
+        student_obj.register_time = datetime.now()
+        student_obj.save()
+        return render_to_response('black.html', {'msg': '修改成功', 'location': 'student.html'})
+
+@csrf_exempt
+def change_teacher_info(req):
+    is_teacher = req.session.get('teacher')
+    if is_teacher is None or not is_teacher:
+        return render_to_response('black.html', {'msg': '请登录', 'location': 'login.html'})
+    username = req.session.get('username')
+    try:
+        teacher_obj = Teacher.objects.get(teacher_num__exact=username)
+    except:
+        return render_to_response('black.html', {'msg': '登录信息发生错误，请重新登录', 'location': 'login.html'})
+    if req.method == 'GET':
+        return render_to_response('change_teacher_info.html', {'teacher': teacher_obj})
+    else:
+        username = req.POST.get('username')
+        name = req.POST.get('name')
+        password = req.POST.get('password')
+        re_password = req.POST.get('re_password')
+        department = req.POST.get('department')
+        if not password == re_password:
+            return render_to_response('change_teacher_info.html', {'msg': '两次输入的密码不一致'})
+        teachers = Teacher.objects.filter(teacher_num__exact=username)
+        if username == '' or name == '' or department == '':
+            return render_to_response('change_teacher_info.html', {'msg': '所有项均为必填项'})
+        teacher_obj.name = name
+        if not password == '':
+            teacher_obj.password = password
+        teacher_obj.department = department
+        teacher_obj.save()
+        return render_to_response('black.html', {'msg': '修改成功', 'location': 'teacher.html'})
